18e590b88bfe5f6a7b53f1ea52ee7c4708b32896
Daya Suvagiya <diya.jivani.diji@gmail.com>
2023-10-09 18:12:29 +0200

    Added routes to the navbar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import styles from "./App.module.css";
 import NavBar from "./components/NavBar";
 import Container from "react-bootstrap/Container";
-import { Route, Switch } from "react-router-dom";
+import {Switch,Route} from 'react-router-dom';
 
 function App() {
   return (
@@ -9,9 +9,11 @@ function App() {
       <NavBar />
       <Container className={styles.Conta}>
         <Switch>
-          <Route exact path="/" render={() => <h1>Home Page</h1>} />
+          <Route exact path="/" render={() => <h1>Home page</h1>} />
           <Route exact path="/aboutus" render={() => <h1>About Us</h1>} />
           <Route exact path="/signin" render={() => <h1>Sign In</h1>} />
+          <Route exact path="/signup" render={() => <h1>Sign Up</h1>} />
+          <Route render={() => <p>Page not found!</p>} />
         </Switch>
       </Container>
     </div>
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,30 +1,51 @@
 import React from "react";
 import { Navbar, Container, Nav } from "react-bootstrap";
 import logo from "./assets/logo.png";
-import styles from "../styles/Nav.module.css"; 
+import styles from "../styles/Nav.module.css";
+import { NavLink } from "react-router-dom";
 
 const NavBar = () => {
   return (
     <Navbar expand="md" fixed="top" className={styles.NavBar}>
       <Container>
-        <Navbar.Brand>
-            <img src={logo} alt='logo' height="60"/>
-        </Navbar.Brand>
+        <NavLink to="/">
+          <Navbar.Brand>
+            <img src={logo} alt="logo" height="60" />
+          </Navbar.Brand>
+        </NavLink>
+
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto text-left">
-            <Nav.Link>
-            <i className="fa-solid fa-house-user"></i>
-            Home</Nav.Link>
-            <Nav.Link><i className="fa-solid fa-users"></i>
-            About Us
-            </Nav.Link>
-            <Nav.Link><i className="fa-solid fa-right-to-bracket"></i>
-            Sign In
-            </Nav.Link>
-            <Nav.Link><i className="fa-solid fa-user-plus"></i>
-            Sign Up
-            </Nav.Link>
+            <NavLink 
+                exact
+                className={styles.NavbarLink}
+                activeClassName={styles.Active}
+                to="/">
+              <i className="fa-solid fa-house-user"></i>
+              Home
+            </NavLink>
+            <NavLink 
+                className={styles.NavbarLink}
+                activeClassName={styles.Active}
+                to="/aboutus">
+              <i className="fa-solid fa-users"></i>
+              About Us
+            </NavLink>
+            <NavLink 
+                className={styles.NavbarLink}
+                activeClassName={styles.Active}
+                to="/signin">
+              <i className="fa-solid fa-right-to-bracket"></i>
+              Sign In
+            </NavLink>
+            <NavLink 
+                className={styles.NavbarLink}
+                activeClassName={styles.Active}
+                to="/signup">
+              <i className="fa-solid fa-user-plus"></i>
+              Sign Up
+            </NavLink>
           </Nav>
         </Navbar.Collapse>
       </Container>
