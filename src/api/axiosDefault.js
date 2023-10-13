7cb650c63fe89e447b572cb9e882cc52471f7465
Daya Suvagiya <diya.jivani.diji@gmail.com>
2023-10-13 16:29:33 +0200

    - Created axios interceptors - Set axios in currentusercontet to access token

--- src/api/axiosDefault.js
+++ src/api/axiosDefault.js
@@ -3,3 +3,6 @@ import axios from "axios";
 axios.defaults.baseURL = "https://react-api-p5-3222a1d91d69.herokuapp.com/";
 axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 axios.defaults.withCredentials = true;
+
+export const axiosReq = axios.create();
+export const axiosRes = axios.create();
