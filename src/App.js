4f09f562cddf5a966184b36661dd00d97a51ef2a
Daya Suvagiya <diya.jivani.diji@gmail.com>
2023-10-13 07:27:12 +0200

    Create Signin page: - Create page using bootstrap - Add style to signup page - Set Event handler - Set Error message

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Container from "react-bootstrap/Container";
 import {Switch,Route} from 'react-router-dom';
 import "./api/axiosDefault";
 import SignUp from "./pages/auth/SignUp";
+import SignIn from "./pages/auth/SignIn";
 
 function App() {
   return (
@@ -13,7 +14,7 @@ function App() {
         <Switch>
           <Route exact path="/" render={() => <h1>Home page</h1>} />
           <Route exact path="/aboutus" render={() => <h1>About Us</h1>} />
-          <Route exact path="/signin" render={() => <h1>Sign In</h1>} />
+          <Route exact path="/signin" render={() => <SignIn />} />
           <Route exact path="/signup" render={() => <SignUp />} />
           <Route render={() => <p>Page not found!</p>} />
         </Switch>
---
+++ src/pages/auth/SignIn.js
@@ -0,0 +1,105 @@
+import React, {useState} from "react";
+import { Link } from "react-router-dom";
+import btnStyles from "../../styles/Button.module.css";
+import appStyles from "../../App.module.css";
+import axios from "axios";
+import styles from "../../styles/SigIn.module.css";
+
+import { Form, Button, Col, Row, Container, Alert, } from "react-bootstrap";
+import { useHistory } from "react-router-dom";
+
+const SignIn = () => {
+
+  const [signInData, setSignInData] = useState({
+      username: "",
+      password: "",
+    });
+  const { username, password } = signInData;
+  const [errors, setError] = useState({});
+
+  const history = useHistory();
+
+  const handleChange = (event) => {
+    setSignInData({
+      ...signInData,
+      [event.target.name]: event.target.value,
+    }); 
+  };
+ 
+  const handleSubmit = async(event) => {
+    event.preventDefault();
+    try {
+      await axios.post("/dj-rest-auth/login/", signInData);
+      history.push("/");
+    } catch (err){
+      setError(err.response?.data)
+    }
+  };
+
+  return (
+    <Row className={styles.Row}>
+      <Col className={styles.SigninMain} md={6} >
+        <Container className={`${appStyles.Content} p-4 `}>
+          <h1 className={styles.Header}>sign in</h1>
+
+          <Form onSubmit={handleSubmit}>
+            <Form.Group controlId="username">
+              <Form.Label className="d-none">Username</Form.Label>
+              <Form.Control 
+                type="text" 
+                name="username" 
+                placeholder="Username"
+                className={styles.UserInput}
+                value={username}
+                onChange={handleChange}
+                />
+            </Form.Group>
+            {errors.username?.map((message, idx) => (
+              <Alert variant="warning" key={idx}>
+                {message}
+              </Alert>
+            ))}
+
+            <Form.Group controlId="password">
+              <Form.Label className="d-none">Password</Form.Label>
+              <Form.Control 
+                type="password" 
+                name="password" 
+                placeholder="Password" 
+                className={styles.UserInput}
+                value={password}
+                onChange={handleChange}
+                />
+            </Form.Group>
+            {errors.password?.map((message, idx) => (
+              <Alert variant="warning" key={idx}>
+                {message}
+              </Alert>
+            ))}
+
+            <Button 
+              type="submit"
+              className={`${btnStyles.Button} ${btnStyles.Wide} ${btnStyles.ButtonColor}`}
+              >
+                Sign In
+            </Button>
+
+            {errors.non_field_errors?.map((message, idx) => (
+              <Alert key={idx} variant="warning" className="mt-3">
+                {message}
+              </Alert>
+            ))}
+
+          </Form>
+        </Container>
+        <Container className={`mt-3 ${appStyles.Content}`}>
+          <Link className={styles.Link} to="/signup">
+            Don't have an account?? <span>Sign Up</span>
+          </Link>
+        </Container>
+      </Col>
+    </Row>
+  );
+};
+
+export default SignIn;
