ae7c17dd2c5ac6deb84fc89295f7adab8e00239a
Daya Suvagiya <diya.jivani.diji@gmail.com>
2023-10-16 16:20:15 +0200

    Complate post page

--- src/pages/posts/PostCreateForm.js
+++ src/pages/posts/PostCreateForm.js
@@ -5,6 +5,9 @@ import Button from "react-bootstrap/Button";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Container from "react-bootstrap/Container";
+import Alert from "react-bootstrap/Alert";
+import Image from "react-bootstrap/Image";
+
 
 import Upload from "../../assets/upload.png";
 
@@ -12,7 +15,6 @@ import styles from "../../styles/PostCreateEditForm.module.css";
 import appStyles from "../../App.module.css";
 import btnStyles from "../../styles/Button.module.css";
 import Asset from "../../components/Asset";
-import { Image } from "react-bootstrap";
 import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
 import { axiosReq} from "../../api/axiosDefaults"
 
@@ -77,6 +79,13 @@ function PostCreateForm() {
           onChange={handleChange}
         />
       </Form.Group>
+        {errors?.title?.map((message, idx) => (
+        <Alert variant="warning" key={idx}>
+          {message}
+        </Alert>
+      ))}
+
+      
       <Form.Group>
         <Form.Label>Content</Form.Label>
         <Form.Control
@@ -87,11 +96,15 @@ function PostCreateForm() {
           onChange={handleChange}
         />
       </Form.Group>
+      {errors?.content?.map((message, idx) => (
+        <Alert variant="warning" key={idx}>
+          {message}
+        </Alert>
+      ))}
 
       <Button
         className={`${btnStyles.Button} ${btnStyles.Wide} ${btnStyles.ButtonColor} ${btnStyles.ButtonSpace} `}
-        onClick={() => {}}
-      >
+        onClick={() => history.goBack()}      >
         Cancel
       </Button>
       <Button 
@@ -142,6 +155,12 @@ function PostCreateForm() {
                 ref={imageInput}
               />
             </Form.Group>
+            {errors?.image?.map((message, idx) => (
+              <Alert variant="warning" key={idx}>
+                {message}
+              </Alert>
+            ))}
+
             <div className="d-md-none">{textFields}</div>
           </Container>
         </Col>
