dfc7e29f585b8ed772c3f4055218b4917e82fca7
Daya Suvagiya <diya.jivani.diji@gmail.com>
2023-10-17 15:38:31 +0200

    Created boiler plate to display posts

--- src/App.js
+++ src/App.js
@@ -7,15 +7,45 @@ import SignUp from "./pages/auth/SignUp";
 import SignIn from "./pages/auth/SignIn";
 import PostCreateForm from "./pages/posts/PostCreateForm";
 import PostPage from "./pages/posts/PostPage";
+import PostsPage from "./pages/posts/PostsPage";
+import { useCurrentUser } from "./contexts/CurrentUserContext";
 
 function App() {
+  const currentUser = useCurrentUser();
+  const profile_id = currentUser?.profile_id || "";
 
   return (
         <div className={styles.App}>
           <NavBar />
           <Container className={styles.Conta}>
-            <Switch>
-              <Route exact path="/" render={() => <h1>Home page</h1>} />
+             <Switch>
+              <Route
+                exact
+                path="/"
+                render={() => (
+                  <PostsPage message="No results found. Adjust the search keyword." />
+                )}
+              />
+              <Route
+                exact
+                path="/feed"
+                render={() => (
+                  <PostsPage
+                    message="No results found. Adjust the search keyword or follow a user."
+                    filter={`owner__followed__owner__profile=${profile_id}&`}
+                  />
+                )}
+              />
+              <Route
+                exact
+                path="/liked"
+                render={() => (
+                  <PostsPage
+                    message="No results found. Adjust the search keyword or like a post."
+                    filter={`likes__owner__profile=${profile_id}&ordering=-likes__created_at&`}
+                  />
+                )}
+              />              
               <Route exact path="/signin" render={() => <SignIn />} />
               <Route exact path="/signup" render={() => <SignUp />} />
               <Route exact path="/posts/create" render={() => <PostCreateForm />} />
