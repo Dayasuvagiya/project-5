beae45c5eaf6559ae1d035563688ba0d5cd2a49f
Daya Suvagiya <diya.jivani.diji@gmail.com>
2023-10-18 11:30:14 +0200

    Create post edit form

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import PostPage from "./pages/posts/PostPage";
 import PostsPage from "./pages/posts/PostsPage";
 import { useCurrentUser } from "./contexts/CurrentUserContext";
 import ListsPage from "./pages/ingredients/ListPage";
+import PostEditForm from "./pages/posts/PostEditForm";
 
 function App() {
   const currentUser = useCurrentUser();
@@ -61,6 +62,7 @@ function App() {
               />
               <Route exact path="/posts/create" render={() => <PostCreateForm />} />
               <Route exact path="/posts/:id" render={() => <PostPage />} />
+              <Route exact path="/posts/:id/edit" render={() => <PostEditForm />} />
               <Route render={() => <p>Page not found!</p>} />
             </Switch>
           </Container>
