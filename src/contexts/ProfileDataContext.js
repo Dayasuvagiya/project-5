1ca06449645c029d785115888d9dd4908fa6ed62
Daya Suvagiya <diya.jivani.diji@gmail.com>
2023-10-20 07:45:59 +0200

    Add handleunfollow function to profiledatacontex - Add  unfollowhelper function to utils - onclick handleunfollow to  profilepage

--- src/contexts/ProfileDataContext.js
+++ src/contexts/ProfileDataContext.js
@@ -2,6 +2,8 @@ import { createContext, useContext, useEffect, useState } from "react";
 import { axiosReq, axiosRes } from "../api/axiosDefaults";
 import { useCurrentUser } from "../contexts/CurrentUserContext";
 import { followHelper } from "../utils/utils";
+import { unfollowHelper } from "../utils/utils";
+
 
 const ProfileDataContext = createContext();
 const SetProfileDataContext = createContext();
@@ -43,6 +45,28 @@ export const ProfileDataProvider = ({ children }) => {
     }
   };
 
+  const handleUnfollow = async (clickedProfile) => {
+    try {
+      await axiosRes.delete(`/followers/${clickedProfile.following_id}/`);
+      setProfileData((prevState) => ({
+        ...prevState,
+        pageProfile: {
+          results: prevState.pageProfile.results.map((profile) =>
+            unfollowHelper(profile, clickedProfile)
+          ),
+        },
+        popularProfiles: {
+          ...prevState.popularProfiles,
+          results: prevState.popularProfiles.results.map((profile) =>
+            unfollowHelper(profile, clickedProfile)
+          ),
+        },
+      }));
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   useEffect(() => {
     const handleMount = async () => {
       try {
@@ -63,8 +87,8 @@ export const ProfileDataProvider = ({ children }) => {
 
   return (
     <ProfileDataContext.Provider value={profileData}>
-      <SetProfileDataContext.Provider value={{ setProfileData, handleFollow }}>
-        {children}
+      <SetProfileDataContext.Provider value={{ setProfileData, handleFollow, handleUnfollow }}>
+  {children}
       </SetProfileDataContext.Provider>
     </ProfileDataContext.Provider>
   );
